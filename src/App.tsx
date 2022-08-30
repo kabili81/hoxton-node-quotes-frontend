71f859a20bd138ae8703a560b7ab20c0c354002c
Kabil Ibrahimi <kabili81@hotmail.com>
2022-08-31 00:36:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [quotes, setQuotes] = useState([])
+
+  useEffect(() =>{
+    fetch(`http://localhost:6789/quotes`)
+    .then(resp => resp.json())
+    .then(quotesFromServer => setQuotes(quotesFromServer))
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Your Quote</h1>
+      <ul>
+        {
+          quotes.map(item => <li>
+            <h2>{item.name}</h2>
+            <h3>{item.quote}</h3>
+          </li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
